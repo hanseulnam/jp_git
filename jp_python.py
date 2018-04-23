6834ba403f944cc16463b07aaf2975a482eadbe9
Hanseul Nam <hanseuln@princeton.edu>
2018-04-23 00:56:59 -0400

    Initial commit

---
+++ jp_python.py
@@ -0,0 +1,17 @@
+import pandas as pd 
+import numpy as np 
+import sys
+import statsmodels.api as sm
+import statsmodels.formula.api as smf
+
+from pandas.tseries.tools import to_datetime
+
+
+def read_data():
+
+
+def main():
+
+
+if __name__ == "__main__":
+  main(sys.argv[1:])
